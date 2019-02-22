fe4399175743470931839e73f97da86345e7dcdd
Vinicius Chan <viniciuschan@hotmail.com>
2019-02-22 13:53:48 -0300

    fix matrix input

--- ex_quadrado_magico.c
+++ ex_quadrado_magico.c
@@ -7,49 +7,57 @@ int main() {
     int soma_diag1 = 0, soma_diag2 = 0;
     int soma = 0, quadrado_magico = 1;
 
-    printf("Digite o tamanho da matriz: ");
-    scanf("%d", &tamanho_matriz);
-    int matriz[tamanho_matriz][tamanho_matriz];
+    printf("Digite o número de linhas da matriz: ");
+    scanf("%d", &lin);
+    printf("Digite o número de colunas da matriz: ");
+    scanf("%d", &col);
+
+    if (lin != col) {
+        printf("A matriz precisa ser quadrada, tente novamente!");
+        exit(0);
+    }
+
+    int matriz[lin][col];
 
     // Alimenta matriz
-    for(i = 0; i < tamanho_matriz; i++){
-        for(j = 0; j < tamanho_matriz; j++){
+    for (i = 0; i < lin; i++) {
+        for (j = 0; j < col; j++) {
             printf("Digite os números: ");
             scanf("%d", &matriz[i][j]);
         }
     }
-    
-    // Soma coluna1
-    for(i = 0; i < col; i++){
+
+    // Soma linha 1 para pivô da soma
+    for (i = 0; i < lin; i++) {
         soma += matriz[i][0];
     }
 
     // Testa linhas
-    for(i=0; i < lin; i++){
+    for (i=0; i < lin; i++) {
         soma_linha = 0;
-        for(j = 0; j < col; j++){
+        for (j = 0; j < col; j++) {
             soma_linha += matriz[0][j];
         }
-        if(soma_linha != soma){
+        if (soma_linha != soma) {
             quadrado_magico = 0;
         }
     }
 
     // Testa colunas
-    for(i = 0; i < col; i++){
+    for (i = 0; i < col; i++) {
         soma_coluna = 0;
-        for(j = 0; j < col; j++){
+        for (j = 0; j < col; j++) {
             soma_coluna += matriz[i][0];
         }
-        if(soma_coluna != soma){
+        if (soma_coluna != soma) {
             quadrado_magico = 0;
         }
     }
 
     // Testa diagonal 1
-    for(i = 0; i < col; i++) {
+    for (i = 0; i < col; i++) {
         soma_diag1 = 0;
-        for(j = 0; j < col; j++) {
+        for (j = 0; j < col; j++) {
             if (i == j) {
                 soma_diag1 += matriz[i][j];
             }
@@ -70,13 +78,19 @@ int main() {
         }
     }
 
-    if (quadrado_magico == 1) {
-        printf("\nÉ um quadrado mágico\n");
-    }
-    else {
-        printf("\nNão é quadrado mágico\n");
+    // Mostra matriz
+    for (i=0; i<lin; i++) {
+        for (j=0; j<col;j++) {
+        printf("%3.d\t",matriz[i][j]);
+        }
+        printf("\n\n");
     }
 
+    if (quadrado_magico == 1) {
+        printf("\n\nÉ quadrado mágico!\n");
+    } else {
+        printf("\n\nNão é quadrado mágico!\n");
+    }
     printf("\n\n");
     getchar();
     return 0;
