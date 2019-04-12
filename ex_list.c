26230c1846045be147faffcd0646ebe7f590269a
Vinicius Chan <viniciuschan@hotmail.com>
2019-04-12 01:39:30 -0300

    add new operations

--- ex_list.c
+++ ex_list.c
@@ -9,6 +9,40 @@ typedef struct List {
     struct List *next;
 } cell;
 
+cell *newElement();
+void insert(cell **head, cell **tail);
+void list(cell *head);
+void destroy(cell **head, cell **tail);
+void clear_list(cell **head, cell **tail);
+
+int main() {
+    int op;
+    cell *head = NULL, *tail = NULL;
+
+    do {
+        printf("MENU:\n(1) Insert\n(2) List\n(3) Delete\n(4) Clear list\n(5) Exit\n");
+        scanf("%d", &op);
+
+        switch(op) {
+            case 1:
+                insert(&head, &tail);
+                break;
+            case 2:
+                list(head);
+                break;
+            case 3:
+                destroy(&head, &tail);
+                break;
+            case 4:
+                clear_list(&head, &tail);
+                break;
+            default:
+                puts("Invalid option");
+                break;
+        }
+    } while (op != 5);
+}
+
 cell *newElement() {
     cell *new;
 
@@ -38,38 +72,39 @@ void insert(cell **head, cell **tail) {
         printf("\n(1) Insert top,\n(2) Insert middle, \n(3) Insert last\n");
         scanf("%d", &op);
 
-    switch(op) {
-        case 1:
-            aux = (*head);
-            (*head) = new;
-            new->next = aux;
-            break;
-        case 2:
-            (*tail)->next = new;
-            (*tail) = new;
-            break;
-        case 3:
-            puts("Choose the position to insert: ");
-            scanf("%s", name);
-
-            aux = (*head);
-
-            while (aux != NULL) {
-                if (strcmp(aux->name, name) == 0) {
-                    temp = aux;
-                    break;
-                }
-                aux = aux->next;
-            }
-
-            if (temp != NULL) {
-                aux = temp->next;
-                temp->next = new;
+        switch(op) {
+            case 1:
+                aux = (*head);
+                (*head) = new;
                 new->next = aux;
-            } else {
-                puts("Invalid option");
-            }
-            break;
+                break;
+            case 2:
+                (*tail)->next = new;
+                (*tail) = new;
+                break;
+            case 3:
+                list(*head);
+                puts("Choose the position to insert: ");
+                scanf("%s", name);
+
+                aux = (*head);
+
+                while (aux != NULL) {
+                    if (strcmp(aux->name, name) == 0) {
+                        temp = aux;
+                        break;
+                    }
+                    aux = aux->next;
+                }
+
+                if (temp != NULL) {
+                    aux = temp->next;
+                    temp->next = new;
+                    new->next = aux;
+                } else {
+                    puts("Invalid option");
+                }
+                break;
         }    
     }
 }
@@ -89,12 +124,53 @@ void list(cell *head) {
     }
 }
 
-int main() {
-    cell *head, *tail;
+void destroy(cell **head, cell **tail) {
+    char name[50];
+    cell *aux = NULL, *temp = NULL;
 
-    // testing
-    insert(&head, &tail);
-    list(head);
+    if (*head == NULL) {
+        puts("Empty list");
+    } else {
+        list(*head);
+        puts("\nType the item to delete: ");
+        scanf("%s", name);
 
-    return 0;
+        if(strcmp((*head)->name, name) == 0) {
+            temp = (*head)->next;
+            free(*head);
+            (*head) = temp;
+        } else {
+            aux = (*head);
+
+            while(aux->next != NULL) {
+                if(strcmp(aux->next->name, name) == 0) {
+                    temp = aux;
+                    break;
+                }
+                aux = aux->next;
+            }
+
+            if(temp != NULL) {
+                if (temp->next == (*tail)) {
+                    (*tail) = temp;
+                }
+                aux = temp->next->next;
+                free(temp->next);
+                temp->next = aux;
+            } else {
+            puts("\nInvalid option");
+            }
+        }
+    }
+}
+
+void clear_list(cell **head, cell **tail) {
+    cell *aux = NULL;
+
+    while((*head) != NULL) {
+        aux = (*head);
+        free(*head);
+        (*head) = aux->next;
+    }
+    free(*tail);
 }
